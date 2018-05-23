50aff216c9aad86333f0b7e5589a57a47a427846
Anthony Albert Siao <tonysiao27@gmail.com>
2018-05-23 20:01:23 +0800

    Initial Solution Setup

---
+++ PetShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PetShop.WebUI.Startup))]
+namespace PetShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
