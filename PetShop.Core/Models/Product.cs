7382455a40bda883864c8cd04daa3c14070216b9
Anthony Albert Siao <tonysiao27@gmail.com>
2018-05-31 17:15:06 +0800

    Implement generic Repository

--- PetShop.Core/Models/Product.cs
+++ PetShop.Core/Models/Product.cs
@@ -9,9 +9,8 @@ using System.Threading.Tasks;
 
 namespace PetShop.Core.Models
 {
-    public class Product
+    public class Product : BaseEntity
     {
-        public string Id { get; set; }
 
         [StringLength(20)]
         [DisplayName("Product Name")]
@@ -24,8 +23,5 @@ namespace PetShop.Core.Models
         public string Category { get; set; }
         public string Image { get; set; }
 
-        public Product(){
-            this.Id = Guid.NewGuid().ToString();
-        }
     }
 }
--- PetShop.WebUI/Controllers/ProductCategoryManagerController.cs
+++ PetShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -10,11 +10,11 @@ namespace PetShop.WebUI.Controllers
 {
     public class ProductCategoryManagerController : Controller
     {
-        ProductCategoryRepository context;
+        InMemoryRepository<ProductCategory> context;
 
         public ProductCategoryManagerController()
         {
-            context = new ProductCategoryRepository();
+            context = new InMemoryRepository<ProductCategory>();
 
         }
         // GET: ProductCategoryManager
