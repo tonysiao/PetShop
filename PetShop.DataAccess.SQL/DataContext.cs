1078bd80ae8d09e4621597b41bb840945d8157fe
Anthony Albert Siao <tonysiao27@gmail.com>
2018-06-04 20:13:20 +0800

    Added Entity Framework

---
+++ PetShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using PetShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            : base("DefaultConnection") {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
