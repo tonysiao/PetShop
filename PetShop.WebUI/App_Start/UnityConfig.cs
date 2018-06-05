eb13e7cd01c1b50e273181c410255507d787330b
Anthony Albert Siao <tonysiao27@gmail.com>
2018-06-05 14:10:19 +0800

    Implement SQL Repository.

--- PetShop.WebUI/App_Start/UnityConfig.cs
+++ PetShop.WebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using PetShop.Core.Contracts;
 using PetShop.Core.Models;
 using PetShop.DataAccess.InMemory;
+using PetShop.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace PetShop.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
     }
 }
